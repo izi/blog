9dc59f9dde58519576b54a37ea9e98d9fbe33576
izi <krzysztof.suchomski@gmail.com>
2018-04-22 16:22:07 +0200

    Fixing unit tests

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { AppRoutingModule } from '../app-routing.module'
+import { PostListComponent } from '../post-list/post-list.component';
+import { ContactComponent } from '../contact/contact.component';
+
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,9 +13,11 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [DashboardComponent, PostListComponent, ContactComponent],
+      imports: [AppRoutingModule],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
